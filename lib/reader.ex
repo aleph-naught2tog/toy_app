d1fabcec20cb1fac8808dfb29fb795726c8182d1
aleph-naught2tog <maggie@8thlight.com>
2018-09-30 00:05:31 -0500

    adds manual stop

--- lib/reader.ex
+++ lib/reader.ex
@@ -1,16 +1,28 @@
 defmodule Reader do
   @spec start(pid) :: :ok
   def start(starter_pid) do
-    new_pid = spawn(fn -> check_mailbox() end)
+    new_pid = spawn(fn -> check_mailbox(starter_pid) end)
     send(starter_pid, {:ok, new_pid})
   end
 
-  @spec check_mailbox(:halt) :: {:ok, :done}
-  @spec check_mailbox(none) :: no_return
-  def check_mailbox(:halt), do: {:ok, :done}
-  def check_mailbox() do
+  @spec check_mailbox(pid) :: no_return
+  def check_mailbox(parent) when is_pid(parent) do
+    current = self()
     receive do
       {sender, :echo_in} -> send(sender, :echo_out)
+      {^parent, :shutdown_start} -> shutdown(parent)
+      {_, {:error, reason}} -> stop(parent, {:error, reason})
+      _ -> stop(parent, {:error, "Unexpected message."})
     end
+  end
+
+  @spec shutdown(pid) :: {:ok, :done}
+  def shutdown(parent_pid) do
+    send(parent_pid, {:ok, :done})
+  end
+
+  @spec stop(pid, {:error, term}) :: {:error, term}
+  def stop(parent_pid, {:error, reason}) do
+    send(parent_pid, {:error, reason})
   end
 end
--- test/toy_app_test.exs
+++ test/toy_app_test.exs
@@ -8,11 +8,25 @@ defmodule ToyAppTest do
       assert is_pid(result)
     end
 
-    test "should respond to message" do
+    test "should respond to echo" do
       current_pid = self()
       {:ok, pid} = Reader.start(current_pid)
       send(pid, {current_pid, :echo_in})
       assert_receive :echo_out
+    end
+
+    test "should stop on error" do
+      current_pid = self()
+      {:ok, pid} = Reader.start(current_pid)
+      send(pid, {current_pid, {:error, "because I said so"}})
+      assert_receive {:error, "because I said so"}
+    end
+
+    test "should stop without error" do
+      current_pid = self()
+      {:ok, pid} = Reader.start(current_pid)
+      send(pid, {current_pid, :shutdown_start})
+      assert_receive {:ok, :done}
     end
   end
 end
